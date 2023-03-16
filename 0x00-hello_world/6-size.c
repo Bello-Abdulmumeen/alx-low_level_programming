fb73843dbe3be23adaea3308d349baad7f93e6bc
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-16 13:25:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float f;
 printf("Size of a char: %lu byte(%)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
