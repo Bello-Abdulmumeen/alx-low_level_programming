07ff97c5f389a8b8ddadc0ebe5a1c811c08521a7
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-16 07:34:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that print a line suing the printf function
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+pringf("with proper grammar, but the outcome is a peice of art,\n")
+	return (0);
+}
