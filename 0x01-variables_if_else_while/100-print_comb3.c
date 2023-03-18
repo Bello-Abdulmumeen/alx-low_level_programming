c31e743ad6725b5450d214a8ef64c5ac08ab69b6
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-18 04:59:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 5; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (j = 0; j < 5; j++)
 		{
 		putchar(i + '0'); /* print the current digit */
 		putchar(j + '0'); /* print the current digit */
