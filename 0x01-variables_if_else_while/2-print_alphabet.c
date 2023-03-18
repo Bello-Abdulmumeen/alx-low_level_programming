5626acaf59180a598513278200e2fa718abdc87b
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-18 01:33:23 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0 (Success)
 */
-int main(void); {
-	char alph[26] = "abcdefghijklmnopqrstuwsyz";
-	int i;
+int main(void)
+{
+        char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+        int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alph[i]);
-	}
-	putchar('\n');
-	return (0);
+        for (i = 0; i < 26; i++)
+        {
+                putchar(alph[i]);
+        }
+        putchar('\n');
+        return (0);
 }
 
