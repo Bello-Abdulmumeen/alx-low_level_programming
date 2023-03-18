53450c3339df3f1d7c30452f1101d63bd4a9361e
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-18 04:20:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+		putchar(i + '0'); /* print the current digit */
+		putchar(i + '0'); /* print the current digit */
+		if (i != 9 || j != 9) /* if not the least combination*/
+		{
+			putchar(','); /* print a comma*/
+			putchar(' '); /* print a space*/
+		}
+		}
+		putchar('\n');
+		return (0);
+}
