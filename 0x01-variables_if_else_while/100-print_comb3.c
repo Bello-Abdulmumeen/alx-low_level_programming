5c7200de08d06a8a57ac01fbd7fa64de7aa0f9ae
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-18 04:53:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 		putchar(i + '0'); /* print the current digit */
 		putchar(j + '0'); /* print the current digit */
 
-		if (i != 9 || j = 9) /* if not the least combination*/
+		if (i != 8) /* if not the least combination*/
 		{
 			putchar(','); /* print a comma*/
 			putchar(' '); /* print a space*/
