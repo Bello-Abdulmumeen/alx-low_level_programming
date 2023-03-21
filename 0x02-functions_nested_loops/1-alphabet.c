9e61f55f7446a95de57f8b2410435e5101e34934
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-21 09:47:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,10 @@
 
 int main(void) 
 {
-	char letter = 'a';
-
-		while (letter <= 'z'); 
-		{
+	char letter 
+		for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-		letter ++;
+
 }
 putchar('\n');
 return (0);
