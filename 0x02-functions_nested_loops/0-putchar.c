eab1b69a66665e6dd0027c510f065d1454b5881f
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-21 13:10:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - print __putchar 
+ * main - print _putchar
  * is written by bwave
  * 
  * Return: 0
 */
 
 int main(void) {
-__putchar('_');
-__putchar('p');
-__putchar('u');
-__putchar('t');
-__putchar('c');
-__putchar('h');
-__putchar('a');
-__putchar('r');
-__putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
