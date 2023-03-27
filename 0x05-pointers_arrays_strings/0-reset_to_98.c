5519a4ecfd415acda9963e4409df6dc9a17078a3
Bello Abdulmumeen <belloabdulmumeen787@gmail.com>
2023-03-27 09:42:27 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** _reset– reset string 
+* @n: parameter function 
+* Return: 0
+*/
+void reset_to_98(int *n)
+{
+int *n;
+n = 402
+
+	for (i = 1; i <= 2; i++) {
+        int num = *(int *)n / 402 + '0';
+        putchar(num);
+        *(int *)n %= 402;
+        *(int *)n += 402;
+	_putchar("%d", n);
+	_putchar("\n");
+    	}
+}
+
